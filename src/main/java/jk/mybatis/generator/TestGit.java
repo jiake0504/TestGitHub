1265126e6c7f4c927f7156f04122f5f344aabe1f
Jackey <jiake0504@qq.com>
2018-10-31 16:08:43 +0800

    third commons test.

--- src/main/java/jk/mybatis/generator/TestGit.java
+++ src/main/java/jk/mybatis/generator/TestGit.java
@@ -3,10 +3,10 @@ package jk.mybatis.generator;
 /**
  * @author jackey
  * @version V1.0
- * @Title: TestGit
- * @Package jk.mybatis.generator
- * @Description: ${todo}(用一句话描述该文件做什么)
- * @date 2018-10-31 13:33
+ * @ Title: TestGit
+ * @ Package jk.mybatis.generator
+ * @ Description: ${todo}(用一句话描述该文件做什么)
+ * @ date 2018-10-31 13:33
  */
 public class TestGit {
     public static void main(String[] args) {
